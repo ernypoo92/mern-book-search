4c1e3bc11b1cfcf4357e02f4bc0acc979538f287
Aaron Jones <ernypoo92@gmail.com>
2022-06-01 22:00:29 -0600

    troubleshooting deployment

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const { ApolloServer } = require( 'apollo-server-express' );
 const path = require('path');
+const { typeDefs, resolvers } = require( './schemas' );
 const db = require('./config/connection');
 const routes = require('./routes');
-const { ApolloServer } = require( 'apollo-server-express' );
 const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require( './schemas' );
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,7 +19,7 @@ const server = new ApolloServer({
 // integrate apollo server with express app as middleware
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
