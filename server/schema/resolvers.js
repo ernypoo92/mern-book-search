bd9c3ef94379bccd64bfb3a833f9245b2b7ff2ba
Aaron Jones <ernypoo92@gmail.com>
2022-06-01 18:20:10 -0600

    added log in and add user mutations in resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -12,6 +12,34 @@ const resolvers = {
         }
         throw new AuthenticationError('You need to be logged in!');
         },
+        Mutation: {
+            addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+        
+            if (!user) {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+        
+            return { token, user };
+            },
+            login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+        
+            if (!user) {
+                throw new AuthenticationError("Incorrect credentials.");
+            }
+        
+            const correctPw = await user.isCorrectPassword(password);
+        
+            if (!correctPw) {
+                throw new AuthenticationError("Incorrect credentials");
+            }
+        
+            const token = signToken(user);
+            return { token, user };
+            },
+        },
     },
 };
 module.exports = resolvers;
