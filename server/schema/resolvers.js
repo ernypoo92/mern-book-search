507c1e2406c21210caefb6f3d872c6792fc17155
Aaron Jones <ernypoo92@gmail.com>
2022-06-01 18:55:04 -0600

    set up front end mutations in utils folder

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -23,6 +23,7 @@ const resolvers = {
         
             return { token, user };
             },
+
             login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
         
@@ -38,6 +39,31 @@ const resolvers = {
         
             const token = signToken(user);
             return { token, user };
+            },
+
+            saveBook: async (parent, { input }, context) => {
+                if (context.user) {
+                    const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                    );
+                    return updatedUser;
+                }
+            
+                throw new AuthenticationError("You need to be logged in.");
+            },
+            
+            removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: args.bookId } } },
+                { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError("You need to be logged in!");
             },
         },
     },
